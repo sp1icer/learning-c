1a109eea428b9e265f69a41c0bb5eecc0a692be5
sp1icer <sp1icersec@gmail.com>
2020-07-28 22:44:34 -0500

    ignoring idea folders, and adding 1_3 start.

--- kAndR/ch_01/1_3.c
+++ kAndR/ch_01/1_3.c
@@ -6,6 +6,17 @@
 */
 
 int main() {
-	int fahr, cels;
-	
+	int currFahr, currCels;
+	int lowerLimit, upperLimit, stepSize;
+
+	lowerLimit = 0;
+	upperLimit = 300;
+	stepSize = 20;
+
+	currFahr = lowerLimit;
+	while (currFahr <= upperLimit) {
+	    currCels = 5 * (currFahr - 32) / 9;
+	    printf("%d\t%d\n", currFahr, currCels);
+	    currFahr += stepSize;
+	}
 }
