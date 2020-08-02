91a3f4d0cb829310b062efc1ef538b3b5be90d9f
sp1icer <sp1icersec@gmail.com>
2020-08-02 12:34:02 -0500

    commenting out debugs

--- assignment_27/malicious.c
+++ assignment_27/malicious.c
@@ -8,16 +8,16 @@ ssize_t write(int fd, const void *buffer, size_t count) {
     ssize_t (*new_write)(int, const void *, size_t);
     new_write = dlsym(RTLD_NEXT, "write");
 
-    printf("[DEBUG] Entered write, created new_write...\n");
+//    printf("[DEBUG] Entered write, created new_write...\n");
 
     if(strcmp(buffer,  "Hello world!") == 0) {
-        printf("[DEBUG] Attempting to compare strings...\n");
+//        printf("[DEBUG] Attempting to compare strings...\n");
         printf("Goodbye, cruel world!\n");
         return(12);
     }
     else
         printf("%s", buffer);
 
-    printf("[DEBUG] Returning result.\n");
+//    printf("[DEBUG] Returning result.\n");
     return(0);
 }
