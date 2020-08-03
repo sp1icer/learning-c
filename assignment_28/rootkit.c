b36b43c2f9e2f508ca88d4f6093292bc264ed9c3
sp1icer <sp1icersec@gmail.com>
2020-08-03 18:57:34 -0500

    Create rootkit.c

---
+++ assignment_28/rootkit.c
@@ -0,0 +1,13 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+
+/*
+ * Goals for rootkit:
+ * - open backdoor
+ * - hide connections from netstat
+ * - hide directory of files from ls/find/other ways to list files
+ * 
+ *
+ * Functions to overwrite:
+ *  -
+ */
